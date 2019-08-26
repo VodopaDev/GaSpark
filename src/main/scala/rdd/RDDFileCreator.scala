bfbf22d37e7d980347d1fd75098d3c7e25cb4f7e
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2019-08-26 20:01:36 +0200

    #4 Documentation for RDDFileCreator

--- src/main/scala/rdd/RDDFileCreator.scala
+++ src/main/scala/rdd/RDDFileCreator.scala
@@ -9,9 +9,12 @@ import dataentry.{GasDataEntry, GasType, StationType}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
+/**
+ * Main object to parse the sanitized XML files into an exploitable RDD
+ */
 private object RDDFileCreator extends App{
 
-  val appName = "GaSpark"
+  val appName = "GaSpark-RDD-Creator"
   val numberOfThreads = Runtime.getRuntime.availableProcessors()
   val conf = new SparkConf()
     .setAppName(appName)
@@ -20,6 +23,12 @@ private object RDDFileCreator extends App{
   val sp = SparkSession.builder().config(conf).getOrCreate()
   import sp.implicits._
 
+  /**
+   * Parse the XML file containing yearly data into an exploitable RDD
+   * @param year year data to parse
+   * @param xmlPath path of the XML files
+   * @param rddPath destination path of the RDD
+   */
   def createYearlyRDD(year: Int, xmlPath: String = "resources/sanitized/", rddPath: String = "resources/rdd/"): Unit = {
     val begin = System.currentTimeMillis()
     sp.read
@@ -52,7 +61,5 @@ private object RDDFileCreator extends App{
     println(s"Creating RDD-$year took ${time}ms")
   }
 
-
   (2007 to 2019).foreach(createYearlyRDD(_))
-
 }
