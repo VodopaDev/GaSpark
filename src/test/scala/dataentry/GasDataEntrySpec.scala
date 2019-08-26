4c58f0d062b59b86fc2b1d0c071b89bd989c65a8
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2019-08-26 13:10:57 +0200

    Tests for GasDataEntry

---
+++ src/test/scala/dataentry/GasDataEntrySpec.scala
@@ -0,0 +1,37 @@
+package dataentry
+
+import dataentry.GasDataEntry._
+import org.scalatest.WordSpec
+
+class GasDataEntrySpec extends WordSpec{
+
+  "GasDataEntry" should {
+    "have correct values" when {
+
+      "created from a RDD string" in {
+        val entry = fromRDDLine("1;2;A;E10;100;2018-01-31")
+        assert(entry.sellerId == 1)
+        assert(entry.department == 2)
+        assert(entry.stationType == StationType.AUTOROUTE)
+        assert(entry.gasType == GasType.E10)
+        assert(entry.price == 100)
+        assert(entry.date == GasDataEntry.datePrinter.parse("2018-01-31"))
+      }
+
+      "created from a many strings" in {
+        val entry = fromStringArguments("1","2","A","E10","100","2018-01-31")
+        assert(entry.sellerId == 1)
+        assert(entry.department == 2)
+        assert(entry.stationType == StationType.AUTOROUTE)
+        assert(entry.gasType == GasType.E10)
+        assert(entry.price == 100)
+        assert(entry.date == GasDataEntry.datePrinter.parse("2018-01-31"))
+      }
+    }
+
+    "have a correct toString method" in {
+      val entry = fromStringArguments("1","2","A","E10","100","2018-01-31")
+      assert(entry.toString == "1;2;A;E10;100;2018-01-31")
+    }
+  }
+}
