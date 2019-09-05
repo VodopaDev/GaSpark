d5b7e11f4b7cc59dc53161dd9ae30ab46f5e52af
Nicolas Jomeau <nicolas.jomeau@outlook.com>
2019-09-05 16:07:48 +0200

    Command based interface to choose a specific type of query

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,12 @@
 import configuration.{GasTypeCompareConf, StationTypeCompareConf}
 import query.{GasTypeComparisonQuery, StationTypeComparisonQuery}
 
-object Main extends App {
-
-  val conf = new StationTypeCompareConf(args)
-  StationTypeComparisonQuery.computeWithConfig(conf)
-
+object Main {
+  def main(args: Array[String]): Unit = {
+    args.toList match {
+      case "station" +: tail => StationTypeComparisonQuery.computeWithConfig(new StationTypeCompareConf(tail))
+      case "gas" +: tail => GasTypeComparisonQuery.computeWithConfig(new GasTypeCompareConf(tail))
+      case _ => println("Use station or gas as your first parameter (use <command> --help for more details)")
+    }
+  }
 }
